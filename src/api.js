17cef339ed1eca1de4b4be898395a4acaad9c7bc
Karl Arnar Ægisson <karla16@ru.is>
2018-10-10 14:06:20 +0000

    fixed folder locations

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
-const greeting = require('./src/greeting');
+const greeting = require('./greeting');
 const express = require("express");
 const app = express();
 
 
-app.get('/greeting/:name', (req, res) => {
+app.get('./greeting/:name', (req, res) => {
 	res.status(200);
 	res.send(req.params.name);
 });
